1b359bdbd5f0a482bcc63970304b7e47e632e34c
Mikuła <monikamikula77@gmail.com>
2022-05-22 02:09:05 +0200

    Link, List, Text components

---
+++ zad6/react-app/src/Link.js
@@ -0,0 +1,14 @@
+import React, {Component} from
+        'react';
+class ListComponent extends Component{
+    render() {
+        return (
+            <ul>
+                <li>Coffee</li>
+                <li>Tea</li>
+                <li>Milk</li>
+            </ul>
+        );
+    }
+}
+export default ListComponent;
